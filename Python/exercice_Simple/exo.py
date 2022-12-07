07376b077f287278872f54ce29731e70cbbfbfb3
PaixSalim <konatesalimadam@gmail.com>
2022-12-07 12:27:16 +0000

    jour 2

---
+++ Python/exercice_Simple/exo.py
@@ -0,0 +1,9 @@
+# parcourir la liste
+dic={}
+liste=['arnaut', 'Rolant', "Pacome", 'salif', 'Fanta', "sali", 'Armel']
+for i in range(0, len(liste)):
+    k=i*i
+    dic[k]=liste[i]
+print(dic)
+    
+
